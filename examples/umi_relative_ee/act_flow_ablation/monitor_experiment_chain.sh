1034b5ea1e17a12b5e900c8db6e284a0c8c809b1
Zhenghao <zfei@zju.edu.cn>
2026-08-12 14:37:08 +0800

    monitor experiment support sessions

--- examples/umi_relative_ee/act_flow_ablation/monitor_experiment_chain.sh
+++ examples/umi_relative_ee/act_flow_ablation/monitor_experiment_chain.sh
@@ -17,8 +17,11 @@ SESSIONS=(
   umi_arch_confirmation_eval_20260812
   umi_arch_extended_candidates_20260812
   umi_act_l1_100k_companion_20260812
+  umi_official_dp_completion_guard_20260812
+  umi_act_l1_100k_completion_guard_20260812
   umi_official_dp_early_eval_20260812
   umi_act_l1_100k_early_eval_20260812
+  umi_lingbot_prefetch_20260812
 )
 MONITOR_LOG="$ARTIFACT_ROOT/logs/chain_monitor_$(date '+%Y%m%d_%H%M%S').log"
 
